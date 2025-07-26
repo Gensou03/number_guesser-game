88596e0bbefc812fac1b519da4b92c9657d9f798
Gensou03 <somdetphetdoem46@gamil.com>
2025-07-26 22:43:27 +0700

    Implement the get_player_guess function

--- game.py
+++ game.py
@@ -11,6 +11,15 @@ def get_player_guess():
     5. Add error handling for invalid input (e.g., text instead of a number).
     """
     # Student 1: Add your code here
+while True:
+        try:
+            guess = int(input("Enter a number between 1 and 100: "))
+            if 1 <= guess <= 100:
+                return guess
+            else:
+                print("❌ Please enter a number **between 1 and 100**.")
+        except ValueError:
+            print("❌ Invalid input. Please enter a number.")
     pass
 
 def check_guess(secret_number, player_guess):
